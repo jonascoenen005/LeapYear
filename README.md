7c5ee197b13d66104c80b592cab99970de699292
Jonas Coenen <jonas.coenen005@gmail.com>
2019-01-11 19:11:59 +0100

    Initial commit of the app

---
+++ README.md
@@ -0,0 +1,5 @@
+#Leap Year Calculator
+
+This app will calculate if a year is a leap year or not
+
+## installation
---
+++ app/src/main/java/com/example/jonas/leapyear/LeapYearActivity.java
@@ -0,0 +1,13 @@
+package com.example.jonas.leapyear;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LeapYearActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leap_year);
+    }
+}
