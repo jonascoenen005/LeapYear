e82c4897252f1701b6fee8b74b20ac478ce1738a
Jonas Coenen <jonas.coenen005@gmail.com>
2019-01-11 20:21:05 +0100

    adding onclickevent calculate leap year in LeapYearActivity

--- app/src/main/java/com/example/jonas/leapyear/LeapYearActivity.java
+++ app/src/main/java/com/example/jonas/leapyear/LeapYearActivity.java
@@ -1,13 +1,34 @@
 package com.example.jonas.leapyear;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LeapYearActivity extends AppCompatActivity {
+    private TextView tv_result;
+    private EditText et_year_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leap_year);
+
+        tv_result = (TextView) findViewById(R.id.tv_result);
+        et_year_input = (EditText) findViewById(R.id.et_year_input);
+    }
+
+    public void btnCalculateClicked(View view) {
+        String inputYear = et_year_input.getText().toString();
+
+        if (LeapYearCalculator.isLeapYear(Integer.parseInt(inputYear))) {
+            tv_result.setText(getString(R.string.is_leap_year, inputYear));
+            tv_result.setTextColor(Color.GREEN);
+        } else {
+            tv_result.setText(getString(R.string.is_not_leap_year, inputYear));
+            tv_result.setTextColor(Color.RED);
+        }
     }
 }
